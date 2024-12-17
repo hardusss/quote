c5eff6e1b67f199e85c881500ea5cc750104aeb9
Danylo <150669333+hardusss@users.noreply.github.com>
2024-12-18 00:14:53 +0200

    Update client.py

--- api/client.py
+++ api/client.py
@@ -1,35 +1,37 @@
 import requests
-
+import random
+from typing import Dict, Generator
 
 class Client:
     def __init__(self, url: str = "http://api.forismatic.com/api/1.0/") -> None:
         """
-        Class for work with API Forismatic
+        Class for working with the Forismatic API.
         Args:
-            url (str): URL API. Defaults to "http://api.forismatic.com/api/1.0/".
+            url (str): The API URL. Defaults to "http://api.forismatic.com/api/1.0/".
         """
         self.url = url
 
-    def get_data(self) -> dict[str, str]:
+    def get_data(self) -> Generator[Dict[str, str], None, None]:
         """
-        Get data from API
+        Fetches data from the API and yields quotes and their authors.
         Returns:
-            dict: Data form API (format=dict) (data=author, quote)
+            Generator: A generator yielding dictionaries with keys 'author' and 'quote'.
         """
-        params: dict[str, str | int] = {
-            "method": "getQuote",
-            "format": "json",
-            "key": 457653,
-            "lang": "en"
-        }
-
         try:
             for _ in range(11):
+                params = {
+                    "method": "getQuote",
+                    "format": "json",
+                    "key": random.randint(1, 999),  
+                    "lang": "en"
+                }
                 with requests.Session() as session:
                     response = session.get(self.url, params=params)
                     response.raise_for_status()  
-                    response = response.json()
-                    print( {"author": response["quoteAuthor"], "quote": response["quoteText"]}  )
+                    response_json = response.json()  
+
+                    yield {"author": response_json["quoteAuthor"], "quote": response_json["quoteText"]}
+
         except requests.exceptions.RequestException as e:
             print(f"Error: {e}")
             return {}
