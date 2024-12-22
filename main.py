75a64d46df76642142b63dd3e617d713746b0570
Danylo <150669333+hardusss@users.noreply.github.com>
2024-12-22 13:22:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from database.models import ModelQuote
 
 if __name__ == "__main__":
     client = Client()
+    model = ModelQuote()
     for quote_data in client.get_data():
-        model = ModelQuote()
         model.insert_quote(author=quote_data['author'], quote=quote_data['quote'])
